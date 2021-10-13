7f853f50fa5769c0707a11011fced806673c5e08
Shakime <jeykimers.johnson@gmail.com>
2021-10-13 12:45:04 -0600

    [ADD] Se agrega el método DD_Newton en Python y Octave.

---
+++ octave. parcial 2/metodo_trazador_lineal.m
@@ -0,0 +1,34 @@
+function ejemplo()
+     clc; clear;
+     pkg load symbolic;
+     
+     xv = [ 1, 2, 4];
+     yv = [1, 0.5, 0.25];
+     s=traz_lineal(xv, yv);
+  
+end
+
+function s= traz_lineal(xv, yv)
+     syms x;
+     
+     n=length(xv);
+     for i=1:(n-1)
+          x_curr = xv(i);        
+          x_next = xv(i+1);
+          
+          y_curr = yv(i);        
+          y_next = yv(i+1);
+      
+          s(i) =  (y_curr+((y_next-y_curr)/(x_next-x_curr))*(x-x_curr));
+
+          if i==1
+               display("S(0)\n");
+               disp(s(i));
+               continue;
+          end
+          printf ("\nS(%d)\n\n", i-1);
+          disp(s(i));
+     end
+    
+end
+  
